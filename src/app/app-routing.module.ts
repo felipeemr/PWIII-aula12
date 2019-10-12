b2066c77d2a1d0c7b088b116754145c85c43f8a8
Felipe Moraes <felipe_moraes_rocha@gmail.com>
2019-10-11 22:20:02 -0300

    aula12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProdutosComponent} from './produtos/produtos.component';
+import {NovoProdutoComponent} from './novo-produto/novo-produto.component';
+import {ContentComponent} from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path :'produtos', 
+  component :ProdutosComponent
+},
+{
+  path :'novo-produto',
+  component : NovoProdutoComponent
+},
+{
+  path : '',
+  component : ContentComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
